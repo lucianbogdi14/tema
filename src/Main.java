9fe1b64e9ca1735ef0a8f1118632c7e87e23f6f6
Bogdan Lucian <bogdanlucian14@gmail.com>
2020-04-14 19:11:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Lucian");
+        int myInt = 2232;
+        byte myBite = 16;
+        int rezultat = (myBite + myInt);
+        System.out.println(rezultat);
+        System.out.println("REZULTAT ADUNARE=" + (2+2));
+        float rezultatImpartire = myInt / myBite ;
+        System.out.println(rezultatImpartire) ;
+        System.out.println("Rezultat A=" + (-5+8*6));
+        System.out.println("Rezultat B=" + (55+9)%9);
+        double myDouble = (20+(-3*5)/8);
+        System.out.println(myDouble);
+        System.out.println("Rezultat C=" + (20+(-3*5)/8));
+        System.out.println("Rezultat D=" + (5+(15/3)*2-8%3));
+    }
+}
